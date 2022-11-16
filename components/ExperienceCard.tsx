bc42de013dbfd64c95fe14bb912ae863fe5e8b07
Prahlad Timalsina <prahladtimalsina7@gmail.com>
2022-11-16 23:52:34 +0545

    work on exp

---
+++ components/ExperienceCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function ExperienceCard({}: Props) {
+  return (
+    <h className="absolute top-24 uppercase tracking-[20px] text-gray-500  text-2xl">ExperienceCard</h>
+  )
+}
+
+export default ExperienceCard
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Header from "../components/Header";
 import React, { useState } from "react";
 import Hero from "../components/Hero";
 import About from "../components/About";
+import WorkExperience from "../components/WorkExperience";
 
 function Home() {
   return (
@@ -18,6 +19,9 @@ function Home() {
       </section>
       <section id="about" className="snap-center">
         <About />
+      </section>
+      <section id="experience" className="snap-center">
+        <WorkExperience />
       </section>
     </div>
   );
