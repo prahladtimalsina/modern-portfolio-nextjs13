5a34439a561fb2508f453dab14206eb883916e76
prahladtimalsina <prahladtimalsina7@gmail.com>
2022-12-05 22:40:56 +0545

    schema created

---
+++ sanity/schemas/experience.js
@@ -0,0 +1,54 @@
+export default {
+  name: "experience",
+  title: "Experience",
+  type: "document",
+  fields: [
+    {
+      name: "jobtitle",
+      title: "JobTitle",
+      type: "string",
+    },
+
+    {
+      name: "companyImage",
+      title: "CompanyImage",
+      type: "image",
+      options: {
+        hotspost: true,
+      },
+    },
+    {
+      name: "company",
+      title: "Company",
+      type: "text",
+    },
+    {
+      name: "dateStarted",
+      title: "DateStarted",
+      type: "date",
+    },
+    {
+      name: "dateEnded",
+      title: "DateEnded",
+      type: "date",
+    },
+    {
+      name: "isCurrentWorkingHere",
+      title: "IsCurrentWorkingHere",
+      type: "boolean",
+    },
+
+    {
+      name: "technologies",
+      title: "Technologies",
+      type: "array",
+      of: [{ type: "reference", to: { type: "skill" } }],
+    },
+    {
+      name: "points",
+      title: "Points",
+      type: "array",
+      of: [{ type: "string" }],
+    },
+  ],
+};
